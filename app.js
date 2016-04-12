adfa7af2a4fa12d32c757289a38ad0bcb6be6aeb
Vlad Turda <vturda@gmail.com>
2016-04-12 05:35:00 -0400

    HTTP file server responds files from public folder

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// node.js shared canvas using socket.io
+// shared canvas using socket.io
+
+var srv = require('http').createServer(handler);
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+var sio = require('socket.io').listen(srv);
+var port = 8080;
+
+srv.listen(port);
+
+function handler(request, response) {
+  var uri = url.parse(request.url).pathname;
+  var filename = 'public' + uri;
+
+  console.log('file "' + filename + '" was requested.');
+
+  fs.exists(filename, function(exists) {
+      //Incase the filename could not be found
+      if(!exists) {
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("404 Not Found\n");
+        response.end();
+        return;
+      }
+
+      //Incase the filename is a directory, append /index.html
+      if (fs.statSync(filename).isDirectory())
+        filename += '/index.html';
+
+      //Write the file contents to response
+      fs.readFile(filename, "binary", function(err, file) {
+        //Error reading file
+        if(err) {
+          response.writeHead(500, {"Content-Type": "text/plain"});
+          response.write(err + "\n");
+          response.end();
+          return;
+        }
+
+        response.writeHead(200);
+        response.write(file, "binary");
+        response.end();
+      });
+
+    });
+}
